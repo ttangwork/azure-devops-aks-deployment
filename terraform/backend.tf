c9edc9a7d876af7aa9211ff49dc03e3872c49888
Tommy Tang <ttangwork@gmail.com>
2026-01-08 22:19:01 +1100

    fix(tf): removed backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-    backend "azurerm" {
-        resource_group_name  = "manual"
-        storage_account_name = "azuredevopsaks"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
+    backend "azurerm" {}
 }
